5eaa55f24258bd91d01e3922c1b83c2f2bdfa2d0
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-12-15 12:59:58 +0530

    Add files via upload

---
+++ HttpExample/__init__.py
@@ -0,0 +1,19 @@
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}!")
+    else:
+        return func.HttpResponse(
+            "Please pass a name in the query string or in the request body",
+            status_code=400
+        )
