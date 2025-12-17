3c733946a2474eeeeb7043e3c898e5d2c92dba1f
sonalrmeshram <sonalrmeshram@gmail.com>
2025-12-17 10:40:56 +0530

    Update __init__.py

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -65,7 +65,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     <div id="result"></div>
 
     <script>
-        const FUNCTION_KEY = "a49SEeielmdwolhyjL9tdcct8pPhK1nzPBlG93on5Ok5AzFuRbMLUw==";
+        const FUNCTION_KEY = "r7cETprpUinuEW03wte_nEGHnD6f_pY1btDOvLiuEuxyAzFuXpnV3g==";
         const API_URL = `/api/HttpExample?api=true&code=${FUNCTION_KEY}`;
         function getUser() {
             const name = document.getElementById("username").value;
